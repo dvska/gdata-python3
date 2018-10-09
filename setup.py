f18948bc6171a179f6633bc80932bbc397c35fd4
Maciej Urbanski <rooter@kyberian.net>
2018-10-09 18:54:17 +0200

    swap tlslite for tlslite-ng

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 import sys
 from distutils.core import setup
 
-required = ['tlslite', 'lxml']
+required = ['tlslite-ng', 'lxml']
 
 if sys.version_info[:3] < (2, 9, 0):
     raise NotImplemented('Python 3.5+ required, bye-bye')
